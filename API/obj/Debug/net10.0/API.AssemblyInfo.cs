8f04cd330c26367ec64d9717b27f92f403ae8134
Wesley <471735994@qq.com>
2026-08-06 00:18:19 +0800

    刷新

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491ea73a0edce14b81324250e19950d1ae6dd877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d270c55213d0ba0fcb38bda2494befe6bad2ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
